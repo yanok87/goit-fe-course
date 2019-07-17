06c6ea1f1377206121edaa2c89b85e9e638b9e02
Iana Matrosova <iana.matrosova@gmail.com>
2019-07-17 16:27:09 +0300

    hw 4 adone

---
+++ JavaScript/module-04/scripts/task-1.js
@@ -0,0 +1,29 @@
+
+// Расставь отсутствующие this в методах объекта account.
+
+const account = {
+  owner: 'Mango',
+  balance: 24000,
+  discount: 0.1,
+  orders: ['order-1', 'order-2', 'order-3'],
+  changeDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost;
+    this.orders.push(order);
+  },
+};
+
+account.changeDiscount(0.15);
+console.log(account.discount); // 0.15
+
+const ordersData = account.getOrders();
+console.log(ordersData); // ['order-1', 'order-2', 'order-3']
+
+account.addOrder(5000, 'order-4');
+console.log(account.balance); // 19000
+console.log(account.orders); // ['order-1', 'order-2', 'order-3', 'order-4']
