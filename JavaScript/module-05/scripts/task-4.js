1f4ec9a206e9ae508e13a43314218620bbf48121
Iana Matrosova <iana.matrosova@gmail.com>
2019-07-23 16:34:00 +0300

    hw 5 done

---
+++ JavaScript/module-05/scripts/task-4.js
@@ -0,0 +1,43 @@
+// Напиши класс StringBuilder. На вход он получает один параметр -
+// строку, которую записывает в свойство _value.
+
+// Добавь классу следующий функционал:
+
+// Геттер value - возвращает текущее значение поля _value
+// Метод append(str) - получает парметр str (строку) и добавляет ее в конец _value
+// Метод prepend(str) - получает парметр str (строку) и добавляет ее в начало value
+// Метод pad(str) - получает парметр str (строку) и добавляет ее в начало и в
+// конец _value
+
+class StringBuilder {
+  constructor(value) {
+    this._value = value;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  append(str) {
+  this._value = this.value + str
+  }
+
+  prepend(str) {
+    this._value =  str + this.value
+  }
+
+  pad(str) {
+    this._value =  str + this.value + str;
+  }
+}
+
+const builder = new StringBuilder('.');
+
+builder.append('^');
+console.log(builder.value); // '.^'
+
+builder.prepend('^');
+console.log(builder.value); // '^.^'
+
+builder.pad('=');
+console.log(builder.value); // '=^.^='
