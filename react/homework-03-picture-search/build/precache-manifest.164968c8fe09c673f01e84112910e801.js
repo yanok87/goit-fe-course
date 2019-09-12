48f31e6376ab2a6e34c42e449a2f5a8fdb10d4cf
Iana Matrosova <iana.matrosova@gmail.com>
2019-09-12 09:45:50 +0300

    deploy

---
+++ react/homework-03-picture-search/build/precache-manifest.164968c8fe09c673f01e84112910e801.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975ecddffbfde31e5fed09f359e76229",
+    "url": "/goit-fe-course/react/homework-03-picture-search/build/index.html"
+  },
+  {
+    "revision": "a9970f99ff658f2a434e",
+    "url": "/goit-fe-course/react/homework-03-picture-search/build/static/css/main.d49dfa4d.chunk.css"
+  },
+  {
+    "revision": "071f0a74c0a3f27a524c",
+    "url": "/goit-fe-course/react/homework-03-picture-search/build/static/js/2.2c589504.chunk.js"
+  },
+  {
+    "revision": "a9970f99ff658f2a434e",
+    "url": "/goit-fe-course/react/homework-03-picture-search/build/static/js/main.a49c8c47.chunk.js"
+  },
+  {
+    "revision": "0bef4bb1a4e19cc60298",
+    "url": "/goit-fe-course/react/homework-03-picture-search/build/static/js/runtime~main.9f90a589.js"
+  }
+]);
