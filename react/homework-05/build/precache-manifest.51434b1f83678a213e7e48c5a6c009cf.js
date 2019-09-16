9f011cf866c988c0e8f585257afd86b77ce71c15
Iana Matrosova <iana.matrosova@gmail.com>
2019-09-16 18:57:58 +0300

    build

---
+++ react/homework-05/build/precache-manifest.51434b1f83678a213e7e48c5a6c009cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcbd5428b17772ec9542450582059c95",
+    "url": "/goit-fe-course/react/homework-05/build/index.html"
+  },
+  {
+    "revision": "423ef81b46b0dc363ddc",
+    "url": "/goit-fe-course/react/homework-05/build/static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "4038577afd6075bead34",
+    "url": "/goit-fe-course/react/homework-05/build/static/js/2.e3a4af39.chunk.js"
+  },
+  {
+    "revision": "423ef81b46b0dc363ddc",
+    "url": "/goit-fe-course/react/homework-05/build/static/js/main.10665c3f.chunk.js"
+  },
+  {
+    "revision": "5febfa8d6310facaeaab",
+    "url": "/goit-fe-course/react/homework-05/build/static/js/runtime~main.f39a75b0.js"
+  }
+]);
