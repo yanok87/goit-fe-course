50c116ca31f93f31e8d066656bd1311259ed6ce9
Iana Matrosova <iana.matrosova@gmail.com>
2019-09-26 15:35:26 +0300

    fixing build

--- react/homework-04-pet-store/build/precache-manifest.1e39258993364db0d9ca7084da5a0de0.js
+++ react/homework-04-pet-store/build/precache-manifest.1e39258993364db0d9ca7084da5a0de0.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6b2d32430029af1452281bc2c63e3f4b",
+    "revision": "0b0c70bff24ce3db5ff165a4db2d6544",
     "url": "/goit-fe-course/react/homework-04-pet-store/build/index.html"
   },
   {
-    "revision": "5e76a58688a992489729",
-    "url": "/goit-fe-course/react/homework-04-pet-store/build/static/css/main.0a017e4e.chunk.css"
+    "revision": "fcde3bf9a8b0970292b6",
+    "url": "/goit-fe-course/react/homework-04-pet-store/build/static/css/main.4be5db62.chunk.css"
   },
   {
     "revision": "c6da6ae860ab457bae09",
     "url": "/goit-fe-course/react/homework-04-pet-store/build/static/js/2.a25e2deb.chunk.js"
   },
   {
-    "revision": "5e76a58688a992489729",
+    "revision": "fcde3bf9a8b0970292b6",
     "url": "/goit-fe-course/react/homework-04-pet-store/build/static/js/main.8c3cb23d.chunk.js"
   },
   {
--- react/homework-04-pet-store/build/service-worker.js
+++ react/homework-04-pet-store/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-fe-course/react/homework-04-pet-store/build/precache-manifest.9b16c9542e2a4418b0e055dab77dd9e1.js"
+  "/goit-fe-course/react/homework-04-pet-store/build/precache-manifest.1e39258993364db0d9ca7084da5a0de0.js"
 );
 
 self.addEventListener('message', (event) => {
